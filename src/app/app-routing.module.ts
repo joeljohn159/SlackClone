e337335e085af0faeab0d39eed001f13b3045d6a
unknown <joe2@gmail.com>
2023-10-14 08:23:46 +0530

    Added SignIn template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'SignIn',component:SignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
